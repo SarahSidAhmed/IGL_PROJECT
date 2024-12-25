4b5fe00f7cb6efd65ec1d47168b731abeee5dba0
Djouaher Yasmine <my_djouaher@esi.dz>
2024-12-25 20:48:44 +0100

    fixed routes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primaryBlue: '#10217D',
-      },
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
-}
-=======
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
@@ -25,4 +8,3 @@ module.exports = {
   plugins: [],
 }
 
->>>>>>> yasmine
