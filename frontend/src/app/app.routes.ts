2e980e789b444ab5a55400d124b13c28454152de
hako0o00 <la_amini@esi.dz>
2024-12-29 20:04:23 +0000

    Added qr_scan component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { RadioListComponent } from './pages/radios-todo/radio-list/radio-list.co
 import { SoinListComponent } from './pages/soins-todo/soin-list/soin-list.component';
 import { TestListComponent } from './pages/tests-todo/test-list/test-list.component';
 import { SigninComponent } from './pages/signin/signin.component';
+import { QrCardComponent } from './pages/qr-card/qr-card.component';
 
 
 export const routes: Routes = [
@@ -16,7 +17,8 @@ export const routes: Routes = [
   { path: 'create-dpi', component: CreateDpiComponent },
   { path: 'radio-list', component: RadioListComponent },
   { path: 'soin-list', component: SoinListComponent },
-  { path: 'tests-list', component: TestListComponent}
+  { path: 'tests-list', component: TestListComponent},
+  { path: 'qr', component: QrCardComponent}
 ];
 
 
