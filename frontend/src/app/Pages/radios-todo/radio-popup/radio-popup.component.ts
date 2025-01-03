f604c43d7b13e8370ee809c1b4ca77932899ed84
hako0o00 <la_amini@esi.dz>
2025-01-03 18:25:54 +0000

    radio update mostly done

--- frontend/src/app/Pages/radios-todo/radio-popup/radio-popup.component.ts
+++ frontend/src/app/Pages/radios-todo/radio-popup/radio-popup.component.ts
@@ -1,22 +1,59 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RadioUpdateService } from '../../../services/update-radio.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'radio-popup',
+  imports : [FormsModule], 
   templateUrl: './radio-popup.component.html',
   styleUrls: ['./radio-popup.component.scss'],
 })
 export class RadioPopupComponent {
-selectedFileName: any;
-onFileSelected($event: Event) {
-throw new Error('Method not implemented.');
-}
   @Input() patientName!: string;
   @Input() gender!: string;
   @Input() age!: number;
   @Input() patientId!: string;
   @Input() radioNeeded!: string;
-
+  
   @Output() closePopup = new EventEmitter<void>();
+  
+  selectedFileName: any;
+  result: string = '';
+  examDate: string = '';
+  file: File | null = null;
+  
+  constructor(private radioUpdateService: RadioUpdateService) {}
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input?.files?.length) {
+      this.file = input.files[0];
+      this.selectedFileName = this.file.name;
+    }
+  }
+
+  onSubmit(): void {
+    this.examDate = new Date().toISOString(); // Format it as a string
+
+    const examData = {
+      image: null, 
+      result: this.result,
+      exam_date: this.examDate, 
+      radiologist: null, 
+    };
+
+    const patientId = parseInt(this.patientId, 10).toString();
+    console.log('Radiological Exam Data:', examData);
+    this.radioUpdateService.updateRadiologicalExam(patientId, examData).subscribe({
+      next: (response) => {
+        console.log('Update successful:', response);
+        this.onClose();
+      },
+      error: (err) => {
+        console.error('Error updating radiological exam:', err);
+      }
+    });
+  }
 
   onClose(): void {
     this.closePopup.emit();
--- frontend/src/app/services/update-radio.service.ts
+++ frontend/src/app/services/update-radio.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, Observable, throwError  } from 'rxjs';
+
+export interface RadiologicalExamUpdate {
+  result: string;
+  exam_date: string;
+  radiologist: number | null;
+  image: string | null;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class UpdateRadioService {
+export class RadioUpdateService {
+  private baseUrl = 'http://127.0.0.1:8000/api/radiological-exams';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  updateRadiologicalExam(id: string, data: RadiologicalExamUpdate): Observable<any> {
+    const url = `${this.baseUrl}/${id}/update/`;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    });
+
+    return this.http.patch(url, data, { headers }).
+    pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: any) {
+    let errorMessage = 'An error occurred';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(() => errorMessage);
+  }
 }
